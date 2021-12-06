51d9c42a451d358964a0c55e0d10cf404bd07ef3
npqhp <19020021@vnu.edu.vn>
2021-12-06 08:18:34 +0700

    Hoàn thành

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,4 +1,8 @@
+import org.json.JSONObject;
+
+import java.io.*;
 import java.net.Socket;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -11,7 +15,10 @@ public class Sensor {
         private Socket socket;
         private final static int BUFFER_SIZE = 1024;
         private final static byte[] BUFFER = new byte[BUFFER_SIZE];
+        private final JSONObject jsonObject = new JSONObject();
+        private double value = -100000;
         public boolean running = true;
+        private boolean disconnect = false;
 
         public SensorLocation(int location, int type) {
             this.location = location;
@@ -19,6 +26,82 @@ public class Sensor {
         }
 
         public void run(){
+            try (Socket sk = new Socket(SERVER_IP, SERVER_PORT)) {
+                this.socket = sk;
+                BufferedWriter os = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                DataInputStream recvFile = new DataInputStream(socket.getInputStream());
+                sendMessage("SENSOR HELO Broker",os);
+                readMessage(recvFile);
+                jsonObject.put("TypeID",String.valueOf(type));
+                jsonObject.put("LocationID",String.valueOf(location));
+
+                while (running && !disconnect) {
+                    sendMessage("INFO",os);
+                    if (disconnect || !running) break;
+                    readMessage(recvFile);
+                    if (disconnect || !running) break;
+
+                    switch (jsonObject.get("TypeID").toString()) {
+                        case "1" -> jsonObject.put("Value", randNum(0, 40));
+                        case "2" -> jsonObject.put("Value", randNum(40, 80));
+                        case "3" -> jsonObject.put("Value", randNum(0, 60));
+                        case "4" -> jsonObject.put("Value", randNum(20, 100));
+                        case "5" -> jsonObject.put("Value", randNum(0, 50));
+                        case "6" -> jsonObject.put("Value", randNum(0.5, 0.7));
+                        case "7" -> jsonObject.put("Value", randNum(0, 100));
+                        case "8" -> jsonObject.put("Value", randNum(0, 55));
+                    }
+
+                    if (disconnect || !running) break;
+                    sendMessage(jsonObject.toString(),os);
+                    if (disconnect || !running) break;
+                    readMessage(recvFile);
+                    if (disconnect || !running) break;
+                    TimeUnit.SECONDS.sleep(5);
+                }
+                sendMessage("QUIT",os);
+                readMessage(recvFile);
+            } catch (IOException ie) {
+                System.out.println("Không thể kết nối đến Server");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private String randNum(double min, double max){
+            if (value == -100000) {
+                Random random = new Random();
+                value = min + random.nextDouble()*(max-min);
+            }
+            Random random = new Random();
+            double t = (max-min)/80;
+            boolean c = random.nextBoolean();
+            double change = random.nextDouble();
+            if (c) value += change*t;
+            else value -= change*t;
+            if (value > max) value = max;
+            if (value < min) value = min;
+            value = (double) ((int) (value*10)) / 10;
+            return String.format("%f",value);
+        }
+
+        private void readMessage(DataInputStream recvFile){
+            try {
+                socket.setSoTimeout(3000);
+                int n = recvFile.read(BUFFER,0,BUFFER_SIZE);
+                if (n < 1) disconnect = true;
+            } catch (IOException ignored) {
+            }
+        }
+
+        private void sendMessage(String message, BufferedWriter os){
+            char[] chars = message.toCharArray();
+            try {
+                os.write(chars,0,chars.length);
+                os.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         private void pause(){
